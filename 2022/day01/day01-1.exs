c1515afd33776acd2d81f88749b70267bc8a1cf2
Enzo <enzo.baldisserri@aworldforus.com>
2022-12-03 13:52:27 +0100

    refactor: use livebooks

--- 2022/day01/day01-1.exs
+++
@@ -1,9 +0,0 @@
-File.stream!("./input.txt")
-|> Stream.map(fn
-  "\n" -> -1
-  line -> line |> String.trim() |> String.to_integer()
-end)
-|> Stream.chunk_by(&(&1 == -1))
-|> Stream.map(&Enum.sum/1)
-|> Enum.max()
-|> dbg()
